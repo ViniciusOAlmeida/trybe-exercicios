031d9b2137279d38038d4328b87e690338a682e2
ViniciusOAlmeida <vinicius.almeida.bio@gmail.com>
2023-02-12 11:36:25 -0300

    Exercício - Clipboard copy

--- FrontEnd/Dia 1 Ambiente de desenvolvimento/Gerador-de-senhas/src/main.js
+++ FrontEnd/Dia 1 Ambiente de desenvolvimento/Gerador-de-senhas/src/main.js
@@ -1,10 +1,16 @@
 import { nanoid } from 'nanoid';
-import './style.css'
+import './style.css';
+import  copy  from 'clipboard-copy';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEL = document.querySelector('h2');
 
 passwordBtnEl.addEventListener('click', () => {
-    const radomPassword = nanoid();
-    displayPasswordEL.innerHTML = radomPassword;
+  const radomPassword = nanoid();
+  displayPasswordEL.innerHTML = radomPassword;
+});
+
+displayPasswordEL.addEventListener('click', (event)=>{
+  copy(event.target.innerHTML);
+  alert('Senha copiada!');
 });
